23fca2725588fb45115b8ce47d6f11cf7a86949c
ManuelGioino <162721952+ManuelGioino@users.noreply.github.com>
2024-05-22 22:25:21 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -2,12 +2,10 @@ def enumerate_list(lista):
     lista_enumerada = []
     counter = 0
     for indice, valor in enumerate(lista):
-        if valor:
-         
+        if valor:    
             lista_enumerada.append(f"{counter}. {valor.title()}")
             counter +=1
     return lista_enumerada
-
 def enumerate_backwards(lista):
     new_list = []
     counter = 0
@@ -16,11 +14,7 @@ def enumerate_backwards(lista):
             new_element = f"{counter}. {valor[::-1]}"
             new_list.append(new_element)
             counter += 1
-            
-
-
     return new_list
-
 colors = ["Red", "Green", "", "White", "Black", ""]
 print(enumerate_list(colors))  
 print(enumerate_backwards(colors)) 
