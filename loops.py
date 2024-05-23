bddd38130e9b09c253bd99c29cead8a6c59cf743
ManuelGioino <162721952+ManuelGioino@users.noreply.github.com>
2024-05-23 18:42:43 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -6,37 +6,39 @@ def index_of_by_index(word, list, index):
         index += 1 
     return -1 
 
-
 def index_of_empty(list):
-     for index, element in enumerate(list):
-        if element == "":
+    index = 0
+    while index < len(list):
+        if list[index] == "":
             return index
-        else:
-            index += 1
-        return -1
-
+        index += 1 
+    return -1   
 
 def index_of(word, list):
-    index = 0 
-    for element in list:
-        if word == element:
+    index = 0
+    while index < len(list):
+        if word == list[index]:
+            return index
+        index += 1 
+    return -1   
+def put(word, list):
+    index = 0
+    while index < len(list):
+        if list[index] == "":
+            list[index] = word
             return index
         index += 1
     return -1
 
-def put(word, list):
-    for indice, valor in enumerate(list):
-        if valor == '':
-            list[indice]=word
-            return indice
-    return -1
+
 
 
 def remove(word, list):
-    count = 0
-    for index, element in enumerate(list):
-        if element == word:
+    index = 0
+    eliminations = 0
+    while index < len(list):
+        if word == list[index]:
             list[index] = ""
-            count += 1
-            return count
-        return -1
+            eliminations += 1
+        index += 1
+    return eliminations
