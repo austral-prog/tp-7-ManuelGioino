5b6b1e11c30b6810839b47f5294b10bd83921d7d
ManuelGioino <162721952+ManuelGioino@users.noreply.github.com>
2024-05-22 22:22:37 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,20 +1,23 @@
-def enumerate_list(colors):
-	lista_enumerada = []
-	for indice, valor in enumerate(colors):
-		if valor: 
-			lista_enumerada.append(f"{len(lista_enumerada)}. {valor.title()}")
-			return lista_enumerada
-colors = ["Red","Green","","White","Black",""]
-lista_enumerada = enumerate_list(colors)
-print(lista_enumerada)
+def enumerate_list(lista):
+    lista_enumerada = []
+    counter = 0
+    for indice, valor in enumerate(lista):
+        if valor:
+         
+            lista_enumerada.append(f"{counter}. {valor.title()}")
+            counter +=1
+    return lista_enumerada
 
+def enumerate_backwards(lista):
+    new_list = []
+    counter = 0
+    for indice, valor in enumerate(lista):
+        if valor:
+            new_element = f"{counter}. {valor[::-1]}"
+            new_list.append(new_element)
+            counter += 1
+    return new_list
 
-def enumerate_backwards(colors):
-    newlist2 = []
-    index2 = 0
-    for element in colors:
-        if len(element) > 0:
-            newlist2.append(f"{index2}. {element [::-1]}")
-            index2 += 1
-    return newlist2
-print(enumerate_backwards(colors))
+colors = ["Red", "Green", "", "White", "Black", ""]
+print(enumerate_list(colors))  
+print(enumerate_backwards(colors)) 
